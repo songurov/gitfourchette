1b65e6f2e82d00a3b6e9765f451261cf5717407a
Songurov Fiodor <songurov@gmail.com>
2026-09-18 09:04:30 +0300

    Rename analysis menu to Data

--- gitfourchette/mainwindow.py
+++ gitfourchette/mainwindow.py
@@ -225,7 +225,7 @@ class MainWindow(QMainWindow):
             "File": _("&File"),
             "Edit": _("&Edit"),
             "View": _("&View"),
-            "Analysis": _("&Anal"),
+            "Analysis": _("&Data"),
             "Repo": _("&Repo"),
             "Commands": _("&Commands"),
             "Mount": _("&Mount"),
